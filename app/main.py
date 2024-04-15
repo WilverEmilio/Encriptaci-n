627967103912ca9d669a70a467e0e324cb6577b4
WilverI <wilb123@umes.edu.gt>
2024-04-15 17:43:11 -0600

    Nuevo metodo para poder utilizar nuevas plantillas

--- app/main.py
+++ app/main.py
@@ -2,12 +2,29 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 import os
+from fastapi import Request, Response
+
+#para poder usar plantillas por medio de Jinja2  
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
 app.mount("/public", StaticFiles(directory="./public/static"),name = "static") 
 
+templates = Jinja2Templates(directory="./public/templates")
+
+
+#metodo para utilizar el html sin templates
+# @app.get("/", response_class=HTMLResponse)
+# def root():
+#     udicacion_plantilla = os.path.abspath("./public/static/html/index.html")
+#     return FileResponse(udicacion_plantilla, status_code=200)
+
+#utilizar el metodo html con templates
 @app.get("/", response_class=HTMLResponse)
-def root():
-    udicacion_plantilla = os.path.abspath("./public/static/html/index.html")
-    return FileResponse(udicacion_plantilla, status_code=200)
+def template(response: Response, request: Request):
+    return templates.TemplateResponse("item.html", {"request": request})
+
+@app.get("/registro", response_class=HTMLResponse)
+def registro(response: Response, request: Request):
+    return templates.TemplateResponse("registro.html", {"request": request})

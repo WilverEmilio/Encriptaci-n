4caddd0056b08b4fd649d9ff88817749aa12a5ba
Hanz Maverick <maverick_ruiz@mesoamericana.edu.gt>
2024-04-16 09:49:15 -0600

    Cambios css y html

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
-import random 
+import random
 from fastapi import Request, Response
 from typing import List
 from fastapi import Form
@@ -12,7 +12,7 @@ from fastapi.params import Depends
 from .conexion import SessionLocal, engine
 from fastapi import FastAPI, Form, Request
 from starlette.responses import RedirectResponse
-#para poder usar plantillas por medio de Jinja2  
+#para poder usar plantillas por medio de Jinja2
 from fastapi.templating import Jinja2Templates
 
 #Lista de palabras asociadas con cada letra del alfabeto
@@ -50,7 +50,7 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-app.mount("/public", StaticFiles(directory="./public/static"),name = "static") 
+app.mount("/public", StaticFiles(directory="./public/static"),name = "static")
 
 templates = Jinja2Templates(directory="./public/templates")
 
@@ -93,7 +93,7 @@ def generar_contrasena_aleatoria(texto):
 def decodificar_contrasena(contrasena_texto):
     contrasena_decodificada = []
     letras_acumuladas = ""
-    
+
     palabras_numeros = {
         "cero": 0,
         "uno": 1,
@@ -135,7 +135,7 @@ def get_db():
     try:
         yield db
     finally:
-        db.close()  
+        db.close()
 
 #plantilla de login/inicio
 @app.get("/", response_class=HTMLResponse) #para ir al modelo de ingreso
@@ -143,35 +143,40 @@ def template(request: Request):
     return templates.TemplateResponse("item.html", {"request": request})
 #función para ingresar al sistema
 @app.post("/")
-async def login(request: Request, 
-                db: Session = Depends(get_db), 
-                username: str = Form(...), 
+async def login(request: Request,
+                db: Session = Depends(get_db),
+                username: str = Form(...),
                 password: str = Form(...)):
     user = db.query(models.Registr).filter(models.Registr.usuario == username).first()
-    
+
     if user:
         user_password = user.contrasena
         bytes_object = bytes.fromhex(user_password)
         text = bytes_object.decode("utf-8")
-        
+
         text_min = text.lower()
-        
+
         password_decoded = decodificar_contrasena(text_min)
-        
+
         password_decoded_str = ''.join(str(elemento) for elemento in password_decoded)
-        
+
         pass_min = password.lower()
-        
+
         print("La contraseña en hexadecimal es: ", user_password)
         print("La contraseña en texto es: ", text)
         print("La cadena en minusculas es: ", text_min)
         print("La contrasñea decodificada es: ", password_decoded)
-        
-        if pass_min == password_decoded_str :
-                return templates.TemplateResponse("ingreso.html", {"request": request})
-        else: 
-            return templates.TemplateResponse("item.html", {"request": request})
-        
+
+        if pass_min == password_decoded_str:
+            # Renderizar la plantilla con el nombre de usuario
+            return templates.TemplateResponse("ingreso.html", {"request": request, "username": username})
+        else:
+            return templates.TemplateResponse("ingreso_fallido.html", {"request": request})
+
+
+@app.get("/loginfallo", response_class=HTMLResponse)
+async def igreso_fallido():
+    return templates.TemplateResponse("ingreso_fallido.html", {"request": request})
 
 #plantilla de registro
 @app.get("/registro", response_class=HTMLResponse)
@@ -181,9 +186,9 @@ def registro(
     return templates.TemplateResponse("registro.html", {"request": request})
 #fucnón para registrar los datos a la hora de la creación del usuario
 @app.post('/registro', response_model=schemas.Crear_Usuario)
-async def registro(request: Request, 
-                   username: str = Form(...), 
-                   password: str = Form(...), 
+async def registro(request: Request,
+                   username: str = Form(...),
+                   password: str = Form(...),
                    email: str = Form(...),
                    db: Session = Depends(get_db)
                    ):
@@ -200,14 +205,14 @@ async def registro(request: Request,
     password_hexadecimal = password_random_final.encode().hex()
     print("Su texto convertido a hexadecimal es:", password_hexadecimal)
 
-    
+
     new_user = models.Registr(usuario = username, contrasena = password_hexadecimal, correo_electronico = email)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
-    
+
     print("usuario:", username)
     print("contraseña:", password)
     print("correo:", email)
-    
+
     return templates.TemplateResponse("item.html", {"request": request})
